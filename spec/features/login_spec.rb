e5efcb4206bd02564708ec059e5492b7fb230135
Apoorva Muralidhara <apoorva_muralidhara@yahoo.com>
2019-06-03 01:23:12 -0400

    Moved stubbing methods into rails_helper.  Perhaps not the most beautiful place for them, but at least it eliminated the code duplication.

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -26,28 +26,4 @@ RSpec.feature "Login" do
     
     expect(page).to have_text('Login successful!')
   end
-
-  private
-
-  def stub_failed_github_api_call
-    stub_request(:get, "https://api.github.com/user/repos").
-      with(headers: { 'Accept' => 'application/vnd.github.v3+json',
-       	              'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       	              'Authorization' => "token #{personal_access_token}",
-       	              'Content-Type'=>'application/json',
-       	              'User-Agent'=>'Octokit Ruby Gem 4.14.0'})
-      .to_return(status: 401, body: '', headers: {})
-  end
-
-  def stub_successful_github_api_call(repo_names:)
-    body = repo_names.map { |name| { name: name } }
-
-    stub_request(:get, "https://api.github.com/user/repos").
-      with(headers: { 'Accept' => 'application/vnd.github.v3+json',
-       	              'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       	              'Authorization' => "token #{personal_access_token}",
-       	              'Content-Type'=>'application/json',
-       	              'User-Agent'=>'Octokit Ruby Gem 4.14.0'})
-      .to_return(status: 200, body: body, headers: {})
-  end
 end
--- spec/features/repos_spec.rb
+++ spec/features/repos_spec.rb
@@ -12,7 +12,9 @@ RSpec.feature "Repos", type: :feature do
 
   scenario "User is logged in" do
     stub_successful_github_api_call(repo_names: repo_names)
+
     login
+
     visit '/repos'
 
     repo_names.each do |repo_name|
@@ -27,17 +29,5 @@ RSpec.feature "Repos", type: :feature do
 
     fill_in 'Personal Access Token', with: personal_access_token
     click_button('Login')
-  end
-
-  def stub_successful_github_api_call(repo_names:)
-    body = repo_names.map { |name| { name: name } }
-
-    stub_request(:get, "https://api.github.com/user/repos").
-      with(headers: { 'Accept' => 'application/vnd.github.v3+json',
-       	              'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-       	              'Authorization' => "token #{personal_access_token}",
-       	              'Content-Type'=>'application/json',
-       	              'User-Agent'=>'Octokit Ruby Gem 4.14.0'})
-      .to_return(status: 200, body: body, headers: {})
   end
 end
