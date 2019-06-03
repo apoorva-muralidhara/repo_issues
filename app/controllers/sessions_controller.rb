968d23c55fe070f4a5ca3c78cccd72924e8e97e9
Apoorva Muralidhara <apoorva_muralidhara@yahoo.com>
2019-06-03 01:04:24 -0400

    Minor refactorings, extracting a few private methods to make controllers look slightly nicer.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    client = Octokit::Client.new(access_token: params[:personal_access_token])
+    client = Octokit::Client.new(access_token: personal_access_token)
 
     begin
       client.repositories
-      session[:personal_access_token] = params[:personal_access_token]
+      session[:personal_access_token] = personal_access_token
       render :create
     rescue Octokit::Error => error
       flash.alert = 'That personal access token is invalid!'
       render :new 
     end
+  end
+
+  private
+
+  def personal_access_token
+    params[:personal_access_token]
   end
 end
