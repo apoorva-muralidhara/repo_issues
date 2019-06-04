c11cecf8fd6fbe4b203a10ee7b0eb364b253481e
Apoorva Muralidhara <apoorva_muralidhara@yahoo.com>
2019-06-03 22:23:12 -0400

    Added a third issue to test data, to be used in testing updated_at sorting.

--- spec/features/issues_spec.rb
+++ spec/features/issues_spec.rb
@@ -1,42 +1,54 @@
 require 'rails_helper'
 
 RSpec.feature "Issues", type: :feature do
+  include ActiveSupport::Testing::TimeHelpers
+  
   let(:personal_access_token) { 'my_very_own_token' }
   let(:repo_names) { %w(travisjeffery/timecop thoughtbot/shoulda-matchers) }
 
+  let(:now) { Time.zone.local(2019, 6, 3, 19, 46, 40) }
+
   let(:earlier) { Time.zone.local(2018, 3, 18, 16, 30, 45).time }
   let(:later) { Time.zone.local(2019, 1, 25, 15, 15, 45).time }
+
+  let(:recently) { Time.zone.local(2019, 6, 1, 19, 17, 45).time }
 
   let(:formatted_earlier_date) { '18/03/2018' }
 
   let(:later_ago_in_words) { '4 months ago' }
 
-  let(:first_title) { 'Gem never works, unfortunately' }
-  let(:second_title) { 'Actually, gem always works, fortunately' }
+  let(:first_title) { 'Gem never works, unfortunately.' }
+  let(:second_title) { 'Actually, gem always works, fortunately.' }
+  let(:third_title) { 'Basically, this gem is amazing!' }
 
   let(:avatar_url) { 'https://avatars1.githubusercontent.com/u/7371?v=4' }
 
   let(:body) do
     [{ title: first_title, created_at: earlier, updated_at: later, assignee: nil },
      { title: second_title, created_at: earlier, updated_at: earlier,
-       assignee: { avatar_url: avatar_url } }]
+       assignee: { avatar_url: avatar_url } },
+     { title: third_title, created_at: earlier, updated_at: recently }]
   end
   
   scenario "Shows issues for repo" do
     stub_successful_github_api_call(repo_names: repo_names)
 
     login
-    click_on_repo_link
+
+    travel_to(now) do
+      click_on_repo_link
     
-    expect(page).to have_text('Issues:')
+      expect(page).to have_text('Issues:')
 
-    expect(page).to have_text(first_title)
-    expect(page).to have_text(second_title)
+      expect(page).to have_text(first_title)
+      expect(page).to have_text(second_title)
+      expect(page).to have_text(third_title)
 
-    expect(page).to have_text(formatted_earlier_date)
-    expect(page).to have_text(later_ago_in_words)
+      expect(page).to have_text(formatted_earlier_date)
+      expect(page).to have_text(later_ago_in_words)
 
-    expect(page).to have_xpath("//img[contains(@src, '#{avatar_url}')]")
+      expect(page).to have_xpath("//img[contains(@src, '#{avatar_url}')]")
+    end
   end
 
   scenario "Can sort issues in ascending alphabetical order by title" do
@@ -47,11 +59,11 @@ RSpec.feature "Issues", type: :feature do
     
     expect(page).to have_text('Issues:')
 
-    expect(page).to have_text(/#{first_title}.*#{second_title}/)
+    expect(page).to have_text(/#{first_title}.*#{second_title}.*#{third_title}/)
 
     click_link('Title')
 
-    expect(page).to have_text(/#{second_title}.*#{first_title}/)
+    expect(page).to have_text(/#{second_title}.*#{third_title}.*#{first_title}/)
   end
 
   scenario "Has link back to repos page" do
